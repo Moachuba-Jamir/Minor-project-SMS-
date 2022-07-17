2ce2aabf54bb9b7d457fb0853cbebc45534d4162
Moachuba <nutjobflix@gmail.com>
2022-07-17 10:57:04 +0530

    signup

---
+++ php/index.php
@@ -0,0 +1,68 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management Systems</title>
+    <link rel="stylesheet" href="../php/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+    <!-- Nav Bar -->
+   <div class="container navContainer">
+        <div class="row subContainer">
+            <div class="col-lg-4 d-flex justify-content-center aligns-items-center">
+                <a href="index.html">
+                    <img src="../Assets/Images/logo.svg" alt="ICFAI university Logo" width="180". height="70">
+                </a>
+            </div>
+            <div class=" listContainer col-lg-8 col-md-12 col-xs-6 xs-pt-5">
+               <ul class="items d-flex justify-content-center aligns-items-center">
+                    <li class="col-xs-3"><a href="index.php">Home</a></li>
+                    <li class="col-xs-3"><a href="">Results</a></li>
+                    <li class="col-xs-3"><a href="">Support</a></li>
+                    <li class="col-xs-3"><a href="">About</a></li>
+               </ul>
+            </div>
+        </div>
+   </div>
+      <!-- Main Body container -->
+ <div class="container main-container">
+    <div class="row">
+        <div class=" firstContainer left col-lg-5 d-flex aligns-items-center justify-content-center">
+            <section class="secContainer">
+                <h3 class="d-flex aligns-items-center justify-content-center">ICFAI Student Management System</h3>
+                <p class="sectionPara d-flex aligns-items-center justify-content-center">Grab your results now! Simply log in with your university Id and download your results for 
+                    <h5 class="c1 animateMe col-lg-9 d-flex aligns-items-center justify-content-center">c1, c2 & End semester Exams</h5>
+                </p>
+                <div class="row mainbtnContainer">
+                    <div class="col-lg-4 col-md-6 col-sm-6 btnContainer ">
+                        <button class="btnn"> &nbsp &nbsp Moachuba &nbsp &nbsp</button>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 btnContainer ">
+                        <button class="btnn">&nbsp &nbsp Log out &nbsp &nbsp</button>
+                    </div>
+                </div>
+                <br>
+            </section>
+        </div>
+        <div class="col-lg-7 d-flex aligns-items-center justify-content-center">
+            <video class="oneVid" src="../Assets/videos/ku2hye5v.mp4" autoplay loop></video>
+        </div>
+    </div>
+    
+ </div>
+<!-- Bootstrap cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="index.js"></script>
+</body>
+
+</html>
